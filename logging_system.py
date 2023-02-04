3e3c9d51d17156baa299845f488f656b5db22b0b
Sascha <67055986+Saschax99@users.noreply.github.com>
2023-02-04 14:32:33 +0100

    1.3.1

--- logging_system.py
+++ logging_system.py
@@ -1,6 +1,7 @@
 import os
 from datetime import datetime
 
+
 class Logging:
     def __init__(self):
         """define variables and standard"""
@@ -13,31 +14,33 @@ class Logging:
         self.folder_name = "logging"
 
         self.__create_log_folder()
-        
+
     def info(self, string):
         """writing info message into file"""
         self.__writing("[INF]", string)
-            
+
     def warning(self, string):
         """writing warning message into file"""
         self.__writing("[WRN]", string)
-            
+
     def __create_log_folder(self):
         """create log folder if not exists"""
         if not os.path.isdir(self.folder_name):
             os.mkdir(self.folder_name)
-            
+
     def __update_file_name(self):
         """updating file name with current date in "%Y-%m-%d" format"""
         if self.date_now != datetime.today().strftime("%Y-%m-%d"):
             self.date_now = datetime.today().strftime("%Y-%m-%d")
-            self.path_file = os.path.join(self.path, self.folder_name, self.file_starting_name + "_" + self.date_now + ".log")
-            
+            self.path_file = os.path.join(self.path, self.folder_name,
+                                          self.file_starting_name + "_" + self.date_now + ".log")
+
     def __writing(self, alarm_type, string):
         """writing into log file """
         self.__update_file_name()
         datetime_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(f'{self.path_file}', 'a', newline='', encoding=self.encoding) as f:
             f.write(datetime_now + " " + alarm_type + ": " + string + "\n")
-            
-logging = Logging()  # assigning logging instance so only activating it once  
+
+
+logging = Logging()  # assigning logging instance so only activating it once
--- main.py
+++ main.py
@@ -1,88 +1,110 @@
-from requests_html import AsyncHTMLSession, HTMLSession
-from string import ascii_letters, digits
+import asyncio
+import csv
+import os.path
+import warnings
 from secrets import choice
+from string import ascii_letters, digits
 from time import time
+
 from colorama import init, Fore
-import csv, os.path, warnings, asyncio, datetime
+from requests_html import AsyncHTMLSession, HTMLSession
+
 from logging_system import logging
+
 warnings.filterwarnings('ignore')
 
-num = 10 # size of string
+num = 10  # size of string
 urls = []
 scrap_url = "https://anonfiles.com/"
 csv_file_name = "data-list.csv"
-amount = 200 # (works fine with 200)
+amount = 200  # (works fine with 200)
 amount_tasks = 0
-text_title='''
+text_title = '''
     ___                      _____ __             _______ __             _______           __         
    /   |  ____  ____  ____  / __(_) /__  _____   / ____(_) /__  _____   / ____(_)___  ____/ /__  _____
   / /| | / __ \/ __ \/ __ \/ /_/ / / _ \/ ___/  / /_  / / / _ \/ ___/  / /_  / / __ \/ __  / _ \/ ___/
  / ___ |/ / / / /_/ / / / / __/ / /  __(__  )  / __/ / / /  __(__  )  / __/ / / / / / /_/ /  __/ /    
 /_/  |_/_/ /_/\____/_/ /_/_/ /_/_/\___/____/  /_/   /_/_/\___/____/  /_/   /_/_/ /_/\__,_/\___/_/                                                                                                                                                                                             
-'''     
+'''
+
 
 def write_into_csv_file(row1, row2, row3):
-    '''write or create csv file'''
+    """write or create csv file"""
     with open(csv_file_name, 'a', newline='', encoding='iso-8859-1') as f:
         writer = csv.writer(f, delimiter=';')
         writer.writerow([row1, row2, row3])
 
-def check_result(res):
-    for i in res:
-        if i != None:
+
+def check_result(result):
+    for i in result:
+        if i is not None:
             print(Fore.GREEN + "Saving " + i + " into CSV..")
             syncDriver = HTMLSession()
             sess = syncDriver.get(i)
             with open(csv_file_name, 'r', newline='', encoding='iso-8859-1') as f:
                 row_count = sum(1 for row in f)
-                title = sess.html.xpath("//h1[@class='text-center text-wordwrap']", first=True).text
+                try:
+                    title = sess.html.xpath("//h1[@class='text-center text-wordwrap']", first=True).text
+                except AttributeError:
+                    title = ""
                 write_into_csv_file(str(row_count), title, sess.url)
                 logging.info(f"saving {sess.url} into {csv_file_name}!")
             syncDriver.close()
-        
+
+
 async def work(driver, url):
     session = await driver.get(url)
     print(Fore.GREEN + "testing url: " + Fore.WHITE + url, end="")
-    if len(session.html.xpath("//h1[@class='text-center']")) > 0 and len(session.html.xpath("//h1[@class='text-center text-wordwrap']")) == 0:
+    if len(session.html.xpath("//h1[@class='text-center']")) > 0 and len(
+            session.html.xpath("//h1[@class='text-center text-wordwrap']")) == 0:
         print(Fore.RED + " doesn't exists")
         return None
     else:
         print(Fore.GREEN + " exists!")
         return session.url
 
+
 async def main(urls):
     global driver
     urls = []
     for _ in range(1, amount):
-        res = ''.join(choice(ascii_letters + digits) for x in range(num)) 
-        urls.append(f''+ scrap_url + str(res))
+        res = ''.join(choice(ascii_letters + digits) for x in range(num))
+        urls.append(f'' + scrap_url + str(res))
     AsyncDriver = AsyncHTMLSession()
     fileURL = (work(AsyncDriver, url) for url in urls)
     return await asyncio.gather(*fileURL)
 
+
 if __name__ == "__main__":
     try:
         init(convert=True)
-        print(text_title + Fore.CYAN + "Anonfiles files finder - By Saschax\n" + Fore.MAGENTA + "\nThe program automatically searches for working Anonfiles files and saves them in the current path as a csv file.\n- CTRL + C to stop program\n")
+        print(
+            text_title + Fore.CYAN + "Anonfiles files finder - By Saschax\n" + Fore.MAGENTA + "\nThe program "
+                                                                                              "automatically searches "
+                                                                                              "for working Anonfiles "
+                                                                                              "files and saves them "
+                                                                                              "in the current path as "
+                                                                                              "a csv file.\n- CTRL + "
+                                                                                              "C to stop program\n")
         if not os.path.isfile(csv_file_name):
             write_into_csv_file("number", "title", "url")
 
         amount = int(input(Fore.MAGENTA + "enter the number of async processes to start the program: \n"))
-        
+
         logging.info("started")
         logging.info(f"amount per period set to {str(amount)}")
-            
+
         while True:
             timer_start = time()
             res = asyncio.run(main(urls))
             check_result(res)
-            print(Fore.MAGENTA + str(round((time()-timer_start)/amount,4)) + " seconds per task")
-            print(Fore.MAGENTA + str(round((time()-timer_start),4)) + " seconds for all " + str(amount) + "tasks")
+            print(Fore.MAGENTA + str(round((time() - timer_start) / amount, 4)) + " seconds per task")
+            print(Fore.MAGENTA + str(round((time() - timer_start), 4)) + " seconds for all " + str(amount) + "tasks")
             amount_tasks = amount_tasks + 1
-            
-            logging.info(f"ended cycle number {amount_tasks} with {amount} tasks within {str(round((time()-timer_start),4))} seconds")
+
+            logging.info(
+                f"ended cycle number {amount_tasks} with {amount} tasks within {str(round((time() - timer_start), 4))} seconds")
 
     except KeyboardInterrupt:
         logging.warning("stopped")
         exit()
-   
